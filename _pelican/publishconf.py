d033b4c72d409ccebbd6ca4f269d7c02d2db36e1
lxnd <lxnd@protonmail.com>
2024-08-02 18:23:55 +0200

    update feed generation and .gitignore

--- _pelican/publishconf.py
+++ _pelican/publishconf.py
@@ -12,7 +12,10 @@ SITEURL = "https://petitalx.github.io"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 DELETE_OUTPUT_DIRECTORY = True
 
